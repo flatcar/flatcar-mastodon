d149d62f36b8013162cd71834f05a4df23cb4cea
Thilo Fromm <thilofromm@microsoft.com>
2022-11-11 16:07:50 +0100

    backup/: backup, restore, and node cloning
    This change adds support for
    - Backup - DB SQL dump, data, and systemd untis are backed up in a
        tarball
    - Restore - system id wiped of Mastodon files, tarball is restored
        to `/` and database is initialised from SQL dump
    - Remote features - designated `backup` user with ssh keys worked into
        provisioning automation
    - Node cloning - deploy a new node that clones a live node, e.g. for
        vertical scaling, switching hosting providers, etc.
    
    A `.gitignore` was added to prevent committing a backup key to the repo.
    
    A few changes and fixes are included for issues encountered during work
       on this change:
    - Grafana dashboards are now downloaded (from the `main` branch of this
       github repo) instead of being included in the user data.
       This works around a data size limitation encountered during testing.
    - mastodon `public/` directory ownership was fixed. Wrong ownership
       prevented user uploads (e.g. pictures, avatars, custom emojis)
    - Initialising the DB on first start-up was removed from the
      `mastodon-db` service file to prevent operational mistakes,
      and made explicit in `re_initialise.sh`
    
    Automation tested on Azure.
    
    Signed-off-by: Thilo Fromm <thilofromm@microsoft.com>

---
+++ backup/re_initialise.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# (Re-)initialise a system from a backup that has been dumped into the root tree.
+
+chown mastodon:mastodon /opt/mastodon/mastodon.env
+
+source /opt/mastodon/versions.env
+chmod 0444 /opt/backup/mastodon_db_backup.sql
+
+echo "Importing mastodon database"
+# We run this in the background instead of using --detach so the log output remains visible
+/usr/bin/docker run --rm  \
+					  --shm-size=256mb \
+					  --health-cmd="pg_isready -U postgres" \
+					  --env POSTGRES_HOST_AUTH_METHOD=trust \
+					  --volume=/opt/backup/mastodon_db_backup.sql:/docker-entrypoint-initdb.d/mastodon_db.sql \
+					  --volume=/opt/mastodon/postgres:/var/lib/postgresql/data \
+					  --name=mastodon-db-import \
+					  "${POSTGRES_IMAGE}" &
+
+# 20 minutes max for db import
+/opt/mastodon/wait-for-container.sh mastodon-db-import .State.Health healthy 1200
+/usr/bin/docker stop mastodon-db-import
+
+rm /opt/backup/mastodon_db_backup.sql
+
+echo "Enabling mastodon and monitoring service units"
+systemctl daemon-reload
+
+# This will be noisy as some of the units come w/o an [Install] section
+# (these are pulled in by other units). Warnings are safe to ignore.
+for f in /etc/systemd/system/mastodon* \
+         /etc/systemd/system/metrics-provider* \
+         /etc/systemd/system/monitoring* \
+         caddy; do
+         u="$(basename "$f")"
+         if [ "$u" = "mastodon-db-import.service" ] ; then
+            continue
+         fi
+         systemctl enable "$u"
+done
+
+echo "Starting mastodon, monitoring, and web server"
+systemctl start --all "mastodon*" "monitoring-*" "metrics-provider-*" caddy
---
+++ backup/restore.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+this_script="/opt/backup/restore.sh"
+
+set -euo pipefail
+
+backup_file="${1:-}"
+
+if [ ! -f "${backup_file}" ] ; then
+    echo "ERROR: missing backup tarball file."
+    echo "Usage: $0 <backup-tarball>"
+    echo "                  <backup-tarball> - backup created with 'backup.sh'"
+    echo
+    exit 1
+fi
+
+# Only sith lords and backup files deal in absolutes.
+backup_file="$(realpath "${backup_file}")"
+
+if    [[ "${backup_file}" == /opt/mastodon/* ]] \
+   || [[ "${backup_file}" == /opt/backup/* ]] \
+   || [[ "${backup_file}" == /opt/caddy/* ]] ; then
+   echo "ERROR: backup file '${backup_file}' resides in a path that wiĺl be WIPED!"
+   echo "       Move outside /opt/mastodon, /opt/backup, or /opt/caddy and try again."
+   echo
+   exit 1
+fi
+
+
+reinit_script="/opt/backup/re_initialise.sh"
+
+if [ "$(realpath "$0")" = "$this_script" ] ; then
+    # The wiping below will also remove this very script, so we create a tempfile to run from.
+    tmp_restore_script="$(mktemp)"
+    chmod 700 "${tmp_restore_script}"
+    cat "${this_script}" >> "${tmp_restore_script}"
+    echo "Moving to tempfile '${tmp_restore_script}'"
+    exec "${tmp_restore_script}" "${@}"
+fi
+
+if [ ! -f "${reinit_script}" ] ; then
+    echo "INTERNAL ERROR: re-init script not present."
+    exit 2
+fi
+
+tmp_reinit_script="$(mktemp)"
+chmod 700 "${tmp_reinit_script}"
+cat "${reinit_script}" >> "${tmp_reinit_script}"
+
+echo "Stopping and disabling all mastodon, monitoring. and web services, and wiping service files."
+
+systemctl stop "mastodon*" "metrics-provider-*" "monitoring*" "caddy*"
+
+for f in /etc/systemd/system/mastodon* \
+         /etc/systemd/system/metrics-provider* \
+         /etc/systemd/system/monitoring* \
+         caddy; do
+         u="$(basename "$f")"
+         # Sadly, "disable" does not support globbing like "start" and "stop" do
+         systemctl disable "$u"
+done
+
+rm -f /etc/systemd/system/mastodon* /etc/systemd/system/metrics-provider-* /etc/systemd/system/monitoring* /etc/systemd/system/caddy*
+
+echo "Removing mastodon, web server, and metrics data."
+rm -rf /opt/mastodon/ /opt/caddy/ /opt/backup/ 
+
+echo "Press return"
+read junk
+
+echo "Restoring backup from '${backup_file}'" 
+cat "${backup_file}"  | docker run --rm -i ghcr.io/flatcar/pigz:latest -d -c - \
+                      | tar x --directory /
+
+echo "Re-initialising system"
+"${tmp_reinit_script}"
